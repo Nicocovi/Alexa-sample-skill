f59d2451e669ad735d29318d5443f26e8c2a3f71
Nicocovi <nico0205@hotmail.com>
2018-02-16 21:03:19 +0100

    Initial commit

---
+++ Server/index.js
@@ -0,0 +1,27 @@
+/* Module dependencies.
+*/
+
+const http = require('http');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static assets from the /public folder
+app.use('/', express.static(path.join(__dirname, 'app')));
+
+/* Get port from environment and store in Express.
+*/
+
+const port = process.env.PORT || '8080';
+app.set('port', port);
+
+/* Create HTTP server.
+*/
+
+const server = http.createServer(app);
+
+/* Listen on provided port, on all network interfaces.
+*/
+
+server.listen(port);
